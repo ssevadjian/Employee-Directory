aae44da056f84b7add8f922cbc2f1453578518b5
SJS <sjsevadjian@gmail.com>
2020-12-20 12:51:12 -0800

    updated searchbox to use bootstrap react components

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBox from "./SearchBox.js";
 import "../styles/Nav.css";
 
+// The SearchBox functional component contains the html for the search box input form
 function Search({ handleSearchChange }) {
   return (
     <nav className="navbar navbar-expand navbar-light bg-light">
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,19 +1,28 @@
 import React from "react";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 import "../styles/SearchBox.css";
 
 function SearchBox({ handleSearchChange }) {
   return (
-    <div className="searchbox">
-      <form className="form-inline">
-        <input
-          className="form-control"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-          onChange={e => handleSearchChange(e)}
-        />
-      </form>
-    </div>
+    <InputGroup size="sm" className="mb-3">
+    <InputGroup.Prepend>
+      <InputGroup.Text id="inputGroup-sizing-sm" onChange={e => handleSearchChange(e)}>Search</InputGroup.Text>
+    </InputGroup.Prepend>
+    <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+  </InputGroup>
+
+    // <div className="searchbox">
+    //   <form className="form-inline">
+    //     <input
+    //       className="form-control"
+    //       type="search"
+    //       placeholder="Search"
+    //       aria-label="Search"
+    //       onChange={e => handleSearchChange(e)}
+    //     />
+    //   </form>
+    // </div>
   );
 }
 export default SearchBox;

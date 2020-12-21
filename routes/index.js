a4bf0143595ec1d2cd567825e43bb3638a03a4cd
SJS <sjsevadjian@gmail.com>
2020-12-20 16:56:34 -0800

    removed unneeded files

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const apiRoutes = require('./apiRoutes');
-const authRoutes = require('./authRoutes');
-// /api/users
-router.use('/api', apiRoutes);
-router.use('/auth', authRoutes);
-
-module.exports = router;
